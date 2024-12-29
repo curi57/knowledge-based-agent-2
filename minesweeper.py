092af1ada6fdefb25b0fc6eaeb22d531f5bd8959
bitficker <shopper.tiago@gmail.com>
2024-12-29 15:13:10 -0300

    creating a .gitignore

--- minesweeper.py
+++ minesweeper.py
@@ -69,7 +69,6 @@ class Sentence():
         self.safes = set()
         self.mines = set()
 
-        #self.__update()
 
     def __eq__(self, other):
         return self.cells == other.cells and self.count == other.count
@@ -84,34 +83,17 @@ class Sentence():
         return self.safes        
         
     def mark_mine(self, cell):
-        if self.cells.__contains__(cell):
+        if cell in self.cells:
             self.mines.add(cell)
             self.cells.remove(cell)
             self.count = self.count - 1
 
     def mark_safe(self, cell):    
-        if self.cells.__contains__(cell):
+        if cell in self.cells:
             self.safes.add(cell)
             self.cells.remove(cell)
 
-    def update(self, safes: set, mines: set):
-        unknowns = len(self.cells)
-                                                                 
-        if self.count == 0 and unknowns > 0:
-            self.safes |= self.cells
-            self.cells.clear()      
-        elif self.count > 0 and self.count == unknowns:
-            self.mines |= self.cells
-            self.cells.clear()
-            self.count = 0
-        else:
-            for safe in safes:
-                self.mark_safe(safe)                             
-            for mine in mines:
-                self.mark_mine(mine)
 
-
-# this class contains all the knowledge about the envinroment
 class MinesweeperAI():
 
     def __init__(self, height=8, width=8):
@@ -134,7 +116,8 @@ class MinesweeperAI():
             sentence.mark_safe(cell)
        
     def extract_knowledge(self, new_sentence: Sentence):
-       
+        
+        self.knowledge.append(new_sentence)
         new_knowledge = None 
         for sentence in self.knowledge:
             subset = None
@@ -151,14 +134,18 @@ class MinesweeperAI():
                 print(f"superset: {superset}")
                 print(f"subset: {subset}") 
 
-                new_knowledge_cells = superset.cells - superset.cells.intersection(subset.cells) 
-                mines_diff = min(superset.count - subset.count, 0)                                                                          
-                new_knowledge = Sentence(new_knowledge_cells, superset.count - mines_diff)
-                #new_knowledge.update(self.safes, self.mines) # esta função deve fornecer informação suficiente para quê outras partes do conhecimento possam se atualizar
+                new_knowledge_cells = superset.cells.difference(subset.cells) 
+                mines_diff = max(superset.count - subset.count, 0)                                                                          
+                new_knowledge = Sentence(new_knowledge_cells, mines_diff)  
+            
+                print(f"new_knowledge before update: {new_knowledge}")
+
                 self.update_knowledge_base(new_knowledge)
 
+                print(f"new_knowledge after update: {new_knowledge}")
+
                 # also check for updates on previous knowledge (after update knowledge base from new knowledge)
-                #self.update_knowledge_base(sentence) -> WHY?!
+                self.update_knowledge_base(sentence) 
 
                 # avoid an infinite loop
                 if not self.knowledge.__contains__(new_knowledge):
@@ -168,40 +155,46 @@ class MinesweeperAI():
         if new_knowledge is not None: 
             self.knowledge.append(new_knowledge)
 
+    def update_knowledge_base(self, sentence: Sentence):
+
+        unknowns = len(sentence.cells)
+        all_safes = sentence.count == 0 and unknowns > 0
+        all_mines = sentence.count > 0 and sentence.count == unknowns
+
+        if all_safes:
+            cp_cells = sentence.cells.copy()
+            for cell in cp_cells:
+                self.mark_safe(cell)  
+
+        elif all_mines:
+            cp_cells = sentence.cells.copy()
+            for cell in cp_cells:
+                self.mark_mine(cell)  
+
+        else:
+            # it just updates the sentence itself. all knowledge required is already known by the other components of the knowledge base
+            sentence.safes |= self.safes
+            sentence.mines |= self.mines
+            #for safe in self.safes:
+            #    sentence.mark_safe(safe)                             
+            #for mine in self.mines:
+            #    sentence.mark_mine(mine)
     
     # add knowledge acquired from the game
     def add_acquired_knowledge(self, cell):
         self.moves_made.add(cell)
         self.mark_safe(cell) 
 
-    def update_knowledge_base(self, sentence: Sentence):
-        
-        #if len(sentence.cells) == 0:
-        #    return False
-        
-        unknowns = len(sentence.cells)
-        if sentence.count == 0 and unknowns > 0:
-            sentence.safes |= sentence.cells
-            sentence.cells.clear()      
-        elif sentence.count > 0 and sentence.count == unknowns:
-            sentence.mines |= sentence.cells
-            sentence.cells.clear()
-            sentence.count = 0
-        else:
-            for safe in self.safes:
-                sentence.mark_safe(safe)                             
-            for mine in self.mines:
-                sentence.mark_mine(mine)
-
-    # remove code repetition 
     def add_knowledge(self, cell, count):
         
         print(f"{cell} - {count}")
-
+        
+        # 1. Atualizando toda a base de conhecimento com uma nova célula 'safe' -> um conhecimento adquirido no jogo e relacionado a uma única célula que foi clicada (adiciona em self.safes e em self.safes em cada Sentence)
         self.add_acquired_knowledge(cell) 
         
         acquired_knowledge = Sentence(self.get_neighboring(cell), count)
-        acquired_knowledge.update(self.safes, self.mines)
+        # 2. Aqui a atualização de conhecimento poderá ser feita com base em inferências explícitas, como o número de minas ser igual a ZERO para todas as células vizinhas
+        self.update_knowledge_base(acquired_knowledge)
         
         self.extract_knowledge(acquired_knowledge)
            
